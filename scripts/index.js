2797c2513ab9db86375b28002dd3d888046b7602
BriannaD23 <98433092+BriannaD23@users.noreply.github.com>
2024-08-22 10:39:30 -0400

    scroll function

--- scripts/index.js
+++ scripts/index.js
@@ -11,28 +11,11 @@ const rightArrow = document.querySelector('.right-arrow');
 
 const scrollAmount = 200; 
 
-function updateArrows() {
-  const maxScrollLeft = containerBox.scrollWidth - containerBox.clientWidth;
-  
-  if (containerBox.scrollLeft <= 0) {
-    leftArrow.disabled = true;
-  } else {
-    leftArrow.disabled = false;
-  }
-
-  if (containerBox.scrollLeft >= maxScrollLeft) {
-    rightArrow.disabled = true;
-  } else {
-    rightArrow.disabled = false;
-  }
-}
-
 function scrollLeft() {
   containerBox.scrollBy({
     left: -scrollAmount,
     behavior: 'smooth'
   });
-  setTimeout(updateArrows, 300);
 }
 
 function scrollRight() {
@@ -40,12 +23,7 @@ function scrollRight() {
     left: scrollAmount,
     behavior: 'smooth'
   });
-  setTimeout(updateArrows, 300);
 }
 
 leftArrow.addEventListener('click', scrollLeft);
 rightArrow.addEventListener('click', scrollRight);
-containerBox.addEventListener('scroll', updateArrows);
-
-// Initial arrow state check
-updateArrows();
