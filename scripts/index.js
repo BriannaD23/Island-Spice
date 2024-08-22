adc8ce5bebd858259d810444907b15543ec4eab5
BriannaD23 <98433092+BriannaD23@users.noreply.github.com>
2024-08-22 10:59:58 -0400

    scrolling

--- scripts/index.js
+++ scripts/index.js
@@ -8,8 +8,21 @@ document.getElementById('menu-icon').addEventListener('click', function(){
 const containerBox = document.querySelector('.container-box');
 const leftArrow = document.querySelector('.left-arrow');
 const rightArrow = document.querySelector('.right-arrow');
+const scrollAmount = 200;
 
-const scrollAmount = 200; 
+function updateArrows() {
+  if (containerBox.scrollLeft === 0) {
+    leftArrow.style.display = 'none';
+  } else {
+    leftArrow.style.display = 'block';
+  }
+
+  if (containerBox.scrollWidth - containerBox.clientWidth === containerBox.scrollLeft) {
+    rightArrow.style.display = 'none';
+  } else {
+    rightArrow.style.display = 'block';
+  }
+}
 
 function scrollLeft() {
   containerBox.scrollBy({
@@ -27,3 +40,6 @@ function scrollRight() {
 
 leftArrow.addEventListener('click', scrollLeft);
 rightArrow.addEventListener('click', scrollRight);
+
+containerBox.addEventListener('scroll', updateArrows);
+updateArrows();
